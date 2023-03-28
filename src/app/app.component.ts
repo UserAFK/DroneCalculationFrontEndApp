6bc0a5230dbc616636f3cd0e269274d6e01291c2
Serhii Molozhenko <molozhenko2011@gmail.com>
2023-03-29 00:02:12 +0300

    mvp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <ul>
+      <li *ngFor="let product of products">{{ product.name }}</li>
+    </ul>
+  `
 })
 export class AppComponent {
-  title = 'DroneCalculationFrontEndApp';
+  public products: any[] | undefined;
+
+  constructor(private apiService: ApiService) {
+    this.apiService.getProducts().subscribe((products: any[]) => {
+      this.products = products;
+    });
+  }
 }
